42b1701996b13d3325846fd5dfb4f80e082206cb
Darian Brown <browndarian011@gmail.com>
2023-10-16 17:03:55 -0400

    slider data

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import React from "react";
 import Navbar from "./components/Navbar";
 import SliderSelect from "./components/SliderSelect";
@@ -7,13 +8,23 @@ import { Container } from "@mui/system";
 import { Grid } from "@mui/material";
 
 function App() {
+  // name, function
+  const [data, setData] = useState({
+    homeValue: 3000,
+    downPayment: 3000,
+    loanAmount: 3000,
+    loanTerm: 3000,
+    interestRate: 5,
+  });
+
+ 
   return (
     <div className="App">
       <Navbar />
       <Container maxWidth="xl" sx={{ mt: 4 }}>
         <Grid container spacing={5}>
           <Grid item md={6} xs={12}>
-            <SliderSelect />
+            <SliderSelect data={data} setData={setData} />
             <TenureSelect />
           </Grid>
 
--- src/components/SliderSelect.js
+++ src/components/SliderSelect.js
@@ -2,44 +2,56 @@ import React from "react";
 import SliderComponent from "./common/SliderComponent";
 import { Stack } from "@mui/material";
 
-const SliderSelect = () => {
+const SliderSelect = ({ data, setData }) => {
+  console.log(data);
+
+  const bank_limit = 10000;
   return (
     <Stack my={1.4}>
       <SliderComponent
         label="Home Value"
         min={1000}
-        max={10000}
-        defaultValue={20}
-        step={5}
+        max={bank_limit}
+        defaultValue={data.homeValue}
+        step={100}
         onChange={(e, value) => {
-          console.log(value);
+          setData({ ...data, homeValue: value });
         }}
-        unit='$'
-        amount={3000}
+        unit="$"
+        amount={data.homeValue}
       />
       <SliderComponent
         label="Down Payment"
-        min={50}
-        max={150}
-        defaultValue={20}
-        step={5}
-        onChange={(e, value) => {
-          console.log(value);
-        }}
-        unit='$'
-        amount={500}
+        min={0}
+        max={data.homeValue}
+        defaultValue={data.downPayment}
+        value={data.downPayment}
+        step={100}
+        onChange={(e, value) => setData({ ...data, downPayment: value })}
+        unit="$"
+        amount={data.downPayment}
       />
       <SliderComponent
         label="Loan Amount"
-        min={150}
-        max={1250}
-        defaultValue={20}
-        step={5}
-        onChange={(e, value) => {
-          console.log(value);
-        }}
-        unit='$'
+        min={0}
+        max={data.homeValue}
+        defaultValue={data.loanAmount}
+        value={data.loanAmount}
+        step={100}
+        onChange={(e, value) => setData({ ...data, loanAmount: value })}
+        unit="$"
         amount={700}
+      />
+      <SliderComponent
+        label="Interest Rate"
+        min={2}
+        max={18}
+        defaultValue={data.interestRate}
+        value={data.interestRate}
+        step={0.1}
+        onChange={(e, value) => setData({ ...data, interestRate: value })}
+        unit="%"
+        amount={data.interestRate}
       />
     </Stack>
   );
