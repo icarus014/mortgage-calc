52221fb619650426074ec12efff1f129f572fc6c
Darian Brown <browndarian011@gmail.com>
2023-10-15 21:46:29 -0400

    finished nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import  AppBar  from '@mui/material/AppBar'
+import  Toolbar  from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
-  )
+    <AppBar position='static'>
+
+        <Toolbar>
+          <Container maxWidth="xl">
+          <Typography variant="h5 " component="div" sx={{ flexGrow: 1 }}>
+              Mortgage Calculator
+          </Typography>
+          </Container>
+        </Toolbar>
+    </AppBar>
+        )
 }
 
 export default Navbar
--- src/components/SliderSelect.js
+++ src/components/SliderSelect.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SliderSelect = () => {
   return (
     <div>
-      
+      SliderSelect
     </div>
   )
 }
