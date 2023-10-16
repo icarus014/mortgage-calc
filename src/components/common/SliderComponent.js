ab92eb127d78738a6be9e5f90c03c0e130512b4b
Darian Brown <browndarian011@gmail.com>
2023-10-15 23:12:47 -0400

    variant change

--- src/components/common/SliderComponent.js
+++ src/components/common/SliderComponent.js
@@ -1,20 +1,24 @@
 import React from "react";
 import Slider from "@mui/material/Slider";
+import { Typography } from "@mui/material";
 
-const SliderComponent = ({defaultValue, min, max, step, onChange, value}) => {
+const SliderComponent = ({ defaultValue, min, max, step, onChange, value }) => {
   return (
     <div>
+      <Typography variant="subtitle1">Home Value</Typography>
+      <Typography variant="h5">$3000</Typography>
       <Slider
         defaultValue={defaultValue}
         min={min}
         max={max}
         aria-label="Default"
         valueLabelDisplay="auto"
-        marks 
-        step={step} 
+        marks
+        step={step}
         onChange={onChange}
         value={value}
       />
+      <Typography variant="h5">$1000</Typography>
     </div>
   );
 };
